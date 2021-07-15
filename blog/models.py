4526d67e2292348709095a3f2fcdb740f5fae4bb
Túlio Farias <tulio-4ever@hotmail.com>
2021-07-14 23:47:47 -0300

    added models.py post class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Post(models.Model):
+    author= models.CharField(max_length=50)
+    text=models.TextField(max_length=3000)
+    date=models.DateTimeField()
+    slug=models.SlugField(default='', blank=True, null=False, db_index=True)
+    
